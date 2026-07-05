56002e82f48e84ad5b76bca19e42a3c850f26c9d
ixx9527 <xiayanele@outlook.com>
2026-07-05 15:25:54 +0800

    feat: 实现悬浮式 custom tabbar（胶囊形状 + 深色/浅色主题 + SVG 线稿图标）

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,55 @@
+Component({
+  data: {
+    selected: 0,
+    theme: 'light',
+    list: [
+      {
+        pagePath: "/pages/index/index",
+        iconPath: "/assets/icons-svg/home_muted_48.svg",
+        selectedIconPath: "/assets/icons-svg/home_primary_48.svg",
+        iconDarkPath: "/assets/icons-svg/home_muted_dark_48.svg",
+        selectedIconDarkPath: "/assets/icons-svg/home_primary_dark_48.svg"
+      },
+      {
+        pagePath: "/pages/error-list/error-list",
+        iconPath: "/assets/icons-svg/book_muted_48.svg",
+        selectedIconPath: "/assets/icons-svg/book_primary_48.svg",
+        iconDarkPath: "/assets/icons-svg/book_muted_dark_48.svg",
+        selectedIconDarkPath: "/assets/icons-svg/book_primary_dark_48.svg"
+      },
+      {
+        pagePath: "/pages/profile/profile",
+        iconPath: "/assets/icons-svg/user_muted_48.svg",
+        selectedIconPath: "/assets/icons-svg/user_primary_48.svg",
+        iconDarkPath: "/assets/icons-svg/user_muted_dark_48.svg",
+        selectedIconDarkPath: "/assets/icons-svg/user_primary_dark_48.svg"
+      }
+    ]
+  },
+  lifetimes: {
+    attached() {
+      // 获取系统主题
+      const systemInfo = wx.getWindowInfo();
+      const theme = systemInfo.theme || 'light';
+      
+      // 获取当前页面路径，设置选中状态
+      const pages = getCurrentPages();
+      const currentPage = pages[pages.length - 1];
+      const currentPath = '/' + currentPage.route;
+      
+      const index = this.data.list.findIndex(item => item.pagePath === currentPath);
+      
+      this.setData({ 
+        selected: index !== -1 ? index : 0,
+        theme: theme
+      });
+    }
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset;
+      const url = data.path;
+      wx.switchTab({ url });
+    }
+  }
+});
